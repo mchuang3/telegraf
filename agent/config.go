3093cb42acbe4eb7f42fc5443bb8363787d5eda8
Mike Chuang <mike.chuang@hpe.com>
2019-08-14 15:16:42 -0500

    Included aggregators, inputs, outputs, processors for config parsing.

--- agent/config.go
+++ agent/config.go
@@ -2,6 +2,11 @@ package agent
 
 import (
 	"github.com/mchuang3/telegraf/internal/config"
+
+	_ "github.com/mchuang3/telegraf/plugins/aggregators/all"
+	_ "github.com/mchuang3/telegraf/plugins/inputs/all"
+	_ "github.com/mchuang3/telegraf/plugins/outputs/all"
+	_ "github.com/mchuang3/telegraf/plugins/processors/all"
 )
 
 // ParseConfig parses the data into a telegraf configuration structure.
